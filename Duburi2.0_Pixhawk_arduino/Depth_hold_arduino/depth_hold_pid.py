7d84d42f9eb93a8ca40a7fae85523d37adc88c7b
Sakib Ahmed Sumdany <ahmadSum1@users.noreply.github.com>
2019-03-06 15:12:23 +0600

    added basic pid controller for Rover

---
+++ Duburi2.0_Pixhawk_arduino/Depth_hold_arduino/depth_hold_pid.py
@@ -0,0 +1,48 @@
+import PID
+import time
+import serial
+
+ser = serial.Serial('/dev/tty.usbserial', 9600)
+
+def mapFromTo(x, a, b, c, d):
+    if x < a:
+        x = a
+    if x > b:
+        x = b
+
+    y = (x - a) / (b - a) * (d - c) + c
+    if x < c:
+        x = c
+    if x > d:
+        x = d
+    return int(y)
+
+
+targetAlt = 100
+currentAlt = 0
+data = [0.0, 0.0]
+
+P = 2
+I = 0.01
+D = 0.5
+
+pid = PID.PID(P, I, D)
+pid.SetPoint = targetAlt
+pid.setSampleTime(1)
+
+
+
+while 1:
+    data = ser.readline().split(' , ')
+    currentAlt = data[0]
+
+    pid.update(currentAlt)
+    output = pid.output
+    # m=interp(output,[-100,100],[1100,1900])  #this uses numpy library
+    throttle = mapFromTo(output, -100, 100, 1200, 1800)  # custom method
+
+    print("Target: %.1f cm | Current: %.1f cm | PPM: %s " % (targetAlt, currentAlt, throttle) + "raw_PID:" + str(output))
+
+    time.sleep(0.5)
+
+
